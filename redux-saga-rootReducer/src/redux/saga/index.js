a48e408db450fdb5a9a4e683fa54448a0c835880
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-05-29 13:36:30 -0300

    added comments for rootsaga config

--- redux-saga-rootReducer/src/redux/saga/index.js
+++ redux-saga-rootReducer/src/redux/saga/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects'
 import userSaga from './userSaga'
 
+//Adicionar dentro do array all todos os sagas que forem criados
 export default function * rootSaga() {
   yield all([userSaga()])
 }
